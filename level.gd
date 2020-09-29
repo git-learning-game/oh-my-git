c178a5c63e46f867615d2dfc5764d7f4629a8c0e
Sebastian Morr <sebastian@morr.cc>
2020-09-29 20:36:57 +0200

    Refactor Level class so that we don't need to copy shell scripts

--- level.gd
+++ level.gd
@@ -28,32 +28,20 @@ func load(path):
 	win_commands = helpers.read_file(path+"/win", "exit 1\n")
 
 func construct():
-	# We're actually destroying stuff here.
-	# Make sure that active_repository is in a temporary directory.
-	helpers.careful_delete(_active_repository_path)
-	helpers.careful_delete(_goal_repository_path)
-	
 	_construct_repo(start_commands +"\n"+ goal_commands, _goal_repository_path)
 	_construct_repo(start_commands, _active_repository_path)
 	
-	var win_script_target = game.tmp_prefix_outside+"/win"
-	helpers.write_file(win_script_target, win_commands)
-	
 func _construct_repo(script_content, path):
-	# Becase in an exported game, all assets are in a .pck file, we need to put
-	# the script somewhere in the filesystem.
-	
-	var script_path_outside = game.tmp_prefix_outside+"/git-hydra-script"
-	var script_path_inside = game.tmp_prefix_inside+"/git-hydra-script"
-	helpers.write_file(script_path_outside, script_content)
+	# We're actually destroying stuff here.
+	# Make sure that active_repository is in a temporary directory.
+	helpers.careful_delete(path)
 	
 	game.global_shell.run("mkdir " + path)
 	game.global_shell.cd(path)
 	game.global_shell.run("git init")
 	game.global_shell.run("git symbolic-ref HEAD refs/heads/main")
-	# Read stdin from /dev/null so that interactive commands don't block.
-	game.global_shell.run("bash "+script_path_inside+" </dev/null")
+	game.global_shell.run(script_content)
 
 func check_win():
 	game.global_shell.cd(_active_repository_path)
-	return game.global_shell.run("bash %s/win 2>/dev/null >/dev/null && echo yes || echo no" % game.tmp_prefix_inside) == "yes\n"
+	return game.global_shell.run("function win { %s }; win 2>/dev/null >/dev/null && echo yes || echo no" % win_commands) == "yes\n"
