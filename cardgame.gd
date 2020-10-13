306e852e734c49ed42a4dd8b38268ed40d4036e3
bleeptrack <mail@bleeptrack.de>
2020-10-13 18:58:30 +0200

    Arrange Cards in Hand

--- cardgame.gd
+++ cardgame.gd
@@ -12,6 +12,7 @@ var cards = [
 	{"command": 'git reflog expire --expire=now --all; git prune', "arg_number": 0},
 	{"command": 'git rebase', "arg_number": 1}
 ]
+var total_angle = 45
 
 func _ready():
 	
@@ -25,10 +26,12 @@ func _ready():
 	game.global_shell.run("git commit --allow-empty -m 'Initial commit'")
 	
 	$Repository.path = path
+	
 
 	$Terminal.repository = $Repository
 	
 	redraw_all_cards()
+	arrange_cards()
 
 func _update_repo():
 	$Repository.update_everything()
@@ -38,12 +41,26 @@ func draw_rand_card():
 	var card = cards[randi() % cards.size()]
 	new_card.command = card.command
 	new_card.arg_number = card.arg_number
-	new_card.position = Vector2(rand_range(200, get_viewport().size.x - 200), get_viewport().size.y*3/4 + rand_range(-200,200))
 	add_child(new_card)
+	arrange_cards()
 	
+func arrange_cards():
+	var amount_cards = get_tree().get_nodes_in_group("cards").size()
+	var angle_between_cards = 0
+	if amount_cards > 1:
+		angle_between_cards = total_angle / (amount_cards-1)
+	var current_angle = -total_angle/2
+	for card in get_tree().get_nodes_in_group("cards"):
+		card.position = Vector2(get_viewport_rect().size.x/2, get_viewport_rect().size.y + 1500)
+		card.rotation_degrees = current_angle
+		var translation_vec = Vector2(0,-1500).rotated(current_angle/180.0*PI)
+		card.position += translation_vec
+		print(current_angle)
+		current_angle += angle_between_cards
+		
 func redraw_all_cards():
 	for card in get_tree().get_nodes_in_group("cards"):
 		card.queue_free()
-	for i in range(10):
+	for i in range(7):
 		draw_rand_card()
 	
